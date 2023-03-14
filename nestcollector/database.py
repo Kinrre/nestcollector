22052d71f257aea189decf75a02bc3a4331782f9
dkmur <dkmur74@gmail.com>
2023-03-14 22:57:17 +0100

    improve logging

--- nestcollector/database.py
+++ nestcollector/database.py
@@ -99,10 +99,10 @@ class Database:
         """
         logging.info('Overwriting multipolygon as polygon...')
         start = time.time()
-        self.db.execute(text('update nests set polygon=ST_CONVEXHULL(polygon) where left(astext(polygon),5) = "MULTI"'))
+        results = self.db.execute(text('update nests set polygon=ST_CONVEXHULL(polygon) where left(astext(polygon),5) = "MULTI"'))
         self.db.commit()
         end = time.time()
-        logging.info(f'Converted all multipolygon to polygon in {human_time(end - start)}.')
+        logging.info(f'Converted {results.rowcount} multipolygon to polygon in {human_time(end - start)}.')
 
     def call_spawnpoints_procedure(self) -> None:
         """
@@ -113,7 +113,8 @@ class Database:
         self.db.execute(text('CALL get_nest_spawnpoints()'))
         self.db.commit()
         end = time.time()
-        logging.info(f'Calculated and filtering spawnpoints of nests in {human_time(end - start)}.')
+        results = self.db.query(Nest).filter(Nest.discarded == "spawnpoints").count()
+        logging.info(f'Calculated and disabled {results} nests low on spawnpoints in {human_time(end - start)}.')
 
     def create_spawnpoints_procedure(self, minimum_spawnpoints: int) -> None:
         """
@@ -140,10 +141,11 @@ class Database:
         """
         logging.info('Disabling overlapping nests...')
         start = time.time()
-        self.db.execute(text('CALL nest_filter_overlap()'))
+        results = self.db.execute(text('CALL nest_filter_overlap()'))
         self.db.commit()
         end = time.time()
-        logging.info(f'Disabled overlapping nests {human_time(end - start)}.')
+        results = self.db.query(Nest).filter(Nest.discarded == "overlap").count()
+        logging.info(f'Disabled {results} overlapping nests in {human_time(end - start)}.')
 
     def create_filtering_procedure(self, maximum_overlap: int) -> None:
         """
